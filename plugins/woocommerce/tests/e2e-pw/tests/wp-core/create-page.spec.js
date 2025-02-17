f257a4c523161eeb84e97931be46c58092ca3ce6
Adrian Moldovan <3854374+adimoldovan@users.noreply.github.com>
2025-02-17 17:37:05 +0200

    [e2e test] Address todos for editor actions with Gutenberg (#55457)
    Remove todo comments on editor actions with Gutenberg

--- plugins/woocommerce/tests/e2e-pw/tests/wp-core/create-page.spec.js
+++ plugins/woocommerce/tests/e2e-pw/tests/wp-core/create-page.spec.js
@@ -31,14 +31,7 @@ test.describe(
 			await fillPageTitle( page, testPage.title );
 
 			const canvas = await getCanvas( page );
-
-			// TODO (Gutenberg 19.9): Remove this click() step.
-			// Current stable version of Gutenberg (19.7) doesn't show the "Empty block" element right away, you need to click on the "Add default block" element first before it appears.
-			// Upcoming Gutenberg nightly (version 19.9) no longer shows the "Add default block" element, but rather displays the "Empty block" right away.
-			// There's no need for this click() step anymore when GB 19.9 comes out.
-			await canvas
-				.getByLabel( /(Add default block|Empty block)/ )
-				.click();
+			await canvas.getByLabel( /Add default block|Empty block/ ).click();
 
 			await canvas
 				.getByRole( 'document', {
