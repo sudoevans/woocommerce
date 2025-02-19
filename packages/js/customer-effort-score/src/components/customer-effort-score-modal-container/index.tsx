0f8d55d492ec378dc6a0738cf1548e0295ee6a55
Chi-Hsuan Huang <chihsuan.tw@gmail.com>
2025-02-19 10:25:14 +0800

    Fix customer-effort-score TS errors and migrate store registration (#55584)
    * Refactor Customer Effort Score store and components
    
    - Update store registration to use createReduxStore and register
    - Remove hardcoded store key references in components
    - Improve type annotations and optional parameters in actions
    - Add missing dependencies to useSelect hooks
    - Remove noCheck option from tsconfig
    
    * Add changelog
    
    * Reformat

--- packages/js/customer-effort-score/src/components/customer-effort-score-modal-container/index.tsx
+++ packages/js/customer-effort-score/src/components/customer-effort-score-modal-container/index.tsx
@@ -12,19 +12,19 @@ import { recordEvent } from '@woocommerce/tracks';
  */
 import { CustomerFeedbackModal } from '../';
 import { getStoreAgeInWeeks } from '../../utils';
-import { STORE_KEY } from '../../store';
 import { ADMIN_INSTALL_TIMESTAMP_OPTION_NAME } from '../../constants';
+import store from '../../store';
 
 export const CustomerEffortScoreModalContainer: React.FC = () => {
 	const { createSuccessNotice } = useDispatch( 'core/notices' );
-	const { hideCesModal } = useDispatch( STORE_KEY );
+	const { hideCesModal } = useDispatch( store );
 	const {
 		storeAgeInWeeks,
 		resolving: isLoading,
 		visibleCESModalData,
 	} = useSelect( ( select ) => {
 		const { getOption, hasFinishedResolution } = select( optionsStore );
-		const { getVisibleCESModalData } = select( STORE_KEY );
+		const { getVisibleCESModalData } = select( store );
 
 		const adminInstallTimestamp =
 			( getOption( ADMIN_INSTALL_TIMESTAMP_OPTION_NAME ) as number ) || 0;
@@ -40,7 +40,7 @@ export const CustomerEffortScoreModalContainer: React.FC = () => {
 			visibleCESModalData: getVisibleCESModalData(),
 			resolving,
 		};
-	} );
+	}, [] );
 
 	const recordScore = (
 		score: number,
